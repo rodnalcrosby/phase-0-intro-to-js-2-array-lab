65775d6b3cc689ff83aada83d9474d4c04d937be
Rodnal Crosby Jr <rodnalcrosby@gmail.com>
2023-10-23 14:53:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name); 
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name); 
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop(); 
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift(); // Remove the first cat from the 'cats' array.
+  }
+  
+  function appendCat(name) {
+    const catsCopy = [...cats]; // Create a copy of the original 'cats' array.
+    catsCopy.push(name); // Append the cat's name to the copy.
+    return catsCopy; // Return the modified copy of the array, leaving the original 'cats' array unchanged.
+  }
+  
+  function prependCat(name) {
+    const catsCopy = [...cats]; // Create a copy of the original 'cats' array.
+    catsCopy.unshift(name); // Prepend the cat's name to the copy.
+    return catsCopy; // Return the modified copy of the array, leaving the original 'cats' array unchanged.
+  }
+  
+  function removeLastCat() {
+    const catsCopy = [...cats]; // Create a copy of the original 'cats' array.
+    catsCopy.pop(); // Remove the last cat from the copy using the pop method.
+    return catsCopy; // Return the modified copy of the array, leaving the original 'cats' array unchanged.
+  }
+  
+  function removeFirstCat() {
+    const catsCopy = [...cats]; // Create a copy of the original 'cats' array.
+    catsCopy.shift(); // Remove the first cat from the copy using the shift method.
+    return catsCopy; // Return the modified copy of the array, leaving the original 'cats' array unchanged.
+  }
+  
